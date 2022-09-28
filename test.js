ed85f71e2b4a21760cc449d8de58a79558ccb6f3
Mangurung29 <114386999+Mangurung29@users.noreply.github.com>
2022-09-28 23:34:47 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 fetch('https://462k64bxqd.execute-api.eu-west-2.amazonaws.com/Stage2/items')
 // fetchRes is the promise to resolve
 // it by using.then() method
-.then(response => response.json());
+.then(response => response.json())
 .then(data => console.log(data));
 
 function details() {
