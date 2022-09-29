9376b28a9f0c3644c8872a1b8f7b6e0518db7cd7
Mangurung29 <114386999+Mangurung29@users.noreply.github.com>
2022-09-29 04:05:48 +0100

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,5 +1,45 @@
-fetch('https://462k64bxqd.execute-api.eu-west-2.amazonaws.com/Stage2/items')
+//fetch('https://462k64bxqd.execute-api.eu-west-2.amazonaws.com/Stage2/items')
 // fetchRes is the promise to resolve
 // it by using.then() method
-.then(response => response.json())
-.then(data => console.log(data));
+//.then(response => response.json())
+//.then(data => console.log(data));
+
+// api url
+const api_url = 
+      "https://462k64bxqd.execute-api.eu-west-2.amazonaws.com/Stage2/items";
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = 
+        `<tr>
+          <th>DateTime</th>
+          <th>Surname</th>
+         </tr>`;
+    
+    // Loop to access all rows 
+    for (let r of data.list) {
+        tab += `<tr> 
+    <td>${r.DateTime} </td>
+    <td>${r.Surname}</td>        
+</tr>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("logs").innerHTML = tab;
+}
