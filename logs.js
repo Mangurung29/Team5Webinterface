59d0f96620dedb4f15dd7d2b0c0a1272819f026a
Mangurung29 <114386999+Mangurung29@users.noreply.github.com>
2022-09-29 08:39:09 +0100

    Update logs.js

--- logs.js
+++ logs.js
@@ -38,7 +38,7 @@ function show(data) {
          </tr>`;
     
     // Loop to access all rows 
-    for (let r of data.list) {
+    for (let r of data.Items) {
         tab += `<tr> 
     <td>${r.DateTime} </td>
     <td>${r.Surname}</td>        
