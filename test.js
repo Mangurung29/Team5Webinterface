c675c99f3325899d15a76ec557c357c3fad4a9e8
Mangurung29 <114386999+Mangurung29@users.noreply.github.com>
2022-09-29 02:25:10 +0100

    Update test.js

--- test.js
+++ test.js
@@ -11,19 +11,10 @@ async function getapi(url) {
     
     // Storing data in form of JSON
     var data = await response.json();
-    console.log(data);
-    if (response) {
-        hideloader();
-    }
-    show(data);
-}
+    
 // Calling that async function
 getapi(api_url);
   
-// Function to hide the loader
-function hideloader() {
-    document.getElementById('loading').style.display = 'none';
-}
 // Function to define innerHTML for HTML table
 function show(data) {
     let tab = 
